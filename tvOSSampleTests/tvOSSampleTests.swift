cbee356750931bc0a8be054bc4bfdf4ba87e22cf
Rad Azzouz <rad@buddybuild.com>
2017-05-09 10:38:40 -0400

    Initial Commit

---
+++ tvOSSampleTests/tvOSSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  tvOSSampleTests.swift
+//  tvOSSampleTests
+//
+//  Created by Rad Azzouz on 2017-05-09.
+//  Copyright © 2017 BuddyBuild. All rights reserved.
+//
+
+import XCTest
+@testable import tvOSSample
+
+class tvOSSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
