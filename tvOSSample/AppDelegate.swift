ec71b7b172c42d8109755ff00f43025e14a9fc77
Rad Azzouz <rad@buddybuild.com>
2017-05-17 11:56:16 -0400

    Removed the buddybuild SDK

--- tvOSSample/AppDelegate.swift
+++ tvOSSample/AppDelegate.swift
@@ -34,7 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
     // MARK: UIApplicationDelegate
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        BuddyBuildSDK.setup()
         
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
